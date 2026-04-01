0cd1c4df1a9d9bedd49429938a03860ac3058af3
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-04-01 09:40:24 +0530

    done

--- class-3/Index.js
+++ class-3/Index.js
@@ -1,34 +1,34 @@
 // // Qus: 1: Write a program to demonstrate the use of if-else statement.
 
-// let a = 7;
-// if (a > 5)
-//     System.out.println("Greater");
-// else
-//     System.out.println("Smaller");
+let a = 7;
+if (a > 5)
+    System.out.println("Greater");
+else
+    System.out.println("Smaller");
 
 // // Qus: 2: Write a program to demonstrate the use of arithmetic operators.
 
-// let x = 50;
-// x /= 5 - 2;
-// console.log(x);
+let x = 50;
+x /= 5 - 2;
+console.log(x);
 
 // // Qus: 3: Write a program to demonstrate the use of assignment operators.
 
-// let y = 7;
-// y += 2;
-// y *= 3;
-// y -= 4;
-// y %= 5;
-// console.log(y);
+let y = 7;
+y += 2;
+y *= 3;
+y -= 4;
+y %= 5;
+console.log(y);
 
 // // Qus: 4: Write a program to demonstrate the use of comparison operators.
 
-// let a = 4 ;
-// a += 2*3 ;
-// console.log(a);
+let b = 4 ;
+b += 2*3 ;
+console.log(b);
 
 // // Qus: 5: Write a program to demonstrate the use of logical operators.
 
-// let y = 45;
-// y /= 4;
-// console.log(y);
+let c = 45;
+c /= 4;
+console.log(c);
